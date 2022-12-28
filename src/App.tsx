29128f0e6c3600db9463dc94718bc61f77969f3a
Matheus Nachtigall <69644855+MatheusNachtigall@users.noreply.github.com>
2022-12-28 18:07:22 -0300

    toggleTheme

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,14 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { ThemeProvider } from "@emotion/react";
-import { LightTheme } from "./shared/themes";
+import { AppThemeProvider } from "./shared/contexts";
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <AppThemeProvider>
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,17 @@
 import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { UseAppThemeContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
+  const { toggleTheme } = UseAppThemeContext();
+
   return (
     <Routes>
       <Route
         path="/pagina-inicial"
         element={
-          <Button variant="contained" color="primary">
-            Teste
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
+            Trocar Tema
           </Button>
         }
       />
